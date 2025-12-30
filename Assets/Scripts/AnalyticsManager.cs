5ffe2c76fc624a65d5d5d177a950fef77ba1e4ae
Roger <roger.puchol@gmail.com>
2025-12-30 21:25:53 +0100

    AnalyticsManager WIP

--- Assets/Scripts/AnalyticsManager.cs
+++ Assets/Scripts/AnalyticsManager.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+
+
+public enum EntityType
+{
+    Player,
+    Enemy,
+    Other
+}
 
 public class AnalyticsManager : MonoBehaviour
 {
+    [Header("Position Tracking")]
+    public bool isPositionTracking = true;
+    public Transform positionTracker;
+    public float sampleRateSeconds = 1f;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,4 +31,47 @@ public class AnalyticsManager : MonoBehaviour
     {
         
     }
+    
+    IEnumerator Upload(Dictionary<string, string> data, string endpoint, Action<uint> callback = null)
+    {
+        WWWForm form = new WWWForm();
+        foreach (var kvp in data)
+        {
+            form.AddField(kvp.Key, kvp.Value);
+        }
+
+        using var www = UnityWebRequest.Post("https://citmalumnes.upc.es/~edgarmd1/" + endpoint, form);
+        www.SetRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!" + www.downloadHandler.text);
+            if (uint.TryParse(www.downloadHandler.text, out var returnValue)){
+                Debug.Log("Callback invoked with value: " + returnValue);
+                callback?.Invoke(returnValue);
+            }
+            else
+                Debug.Log("Error retrieving data: " + www.downloadHandler.text);
+        }
+    }
+
+    #region Data collection delegates
+
+    public void OnPlayerDeath(Transform t)
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        
+        data["time"] = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+        data["position"] = t.position.ToString();
+        data["cause"] = "Unknown"; // TODO modify damageable script to include cause of damage/death
+
+        StartCoroutine(Upload(data, "PlayerDeath"));
+    }
+
+    #endregion
 }
